61f6c7f663afd9db279b164835933ee42a0ddf40
Will Kahn-Greene <willkg@mozilla.com>
2014-08-05 16:08:58 -0400

    Rewrite statsdash
    This is a complete rewrite of statsdash. So far, it just does what it
    did before, but has some additional graph breakdowns.
    
    I included the metrics-graphics stuff, because I plan to use that in
    conjunction with the Input API to pull Input data to get a feel for
    whether things are "normal" or not.

---
+++ statsdash/js/statsdash.js
@@ -0,0 +1,50 @@
+(function($, window) {
+    'use strict';
+
+    var graphiteURL = 'https://graphite-phx1.mozilla.org/render/';
+
+    var graphiteOptions = {
+        height: '400',
+        width: '550',
+        from: '-12hour',
+        hideLegend: 'false'
+    };
+
+    function refreshGraphite() {
+        var items = $('.graphite');
+        var extra = $.param(graphiteOptions) + '&t=' + (new Date()).getTime();
+
+        $.each(items, function(index, value) {
+            var linesToShow = $(value).data('lines');
+            var lines = '';
+
+            $.each(linesToShow, function(index, line) {
+                lines = lines + 'target=' + line + '&';
+            });
+
+            var url = graphiteURL + '?' + lines + extra;
+            $(value).attr('src', url);
+        });
+    }
+
+    var timeoutId = null;
+
+    // Set up to refresh only when the tab is active
+    function runIntervals() {
+        if (window.blurred) {
+            return;
+        }
+        refreshGraphite();
+
+        // Once a minute--run them again.
+        timeoutId = setTimeout(runIntervals, 60000);
+    }
+
+    window.onblur = function() {
+        if (timeoutId !== null) {
+            clearInterval(timeoutId);
+        }
+    };
+    window.onfocus = function() { runIntervals(); };
+
+}(jQuery, window));
